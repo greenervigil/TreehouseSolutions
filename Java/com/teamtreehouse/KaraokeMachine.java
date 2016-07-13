48142eabe9b017f31ed49c621f906089aacd5ab5
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-07-12 21:36:21 -0400

    KAraoke

--- Java/com/teamtreehouse/KaraokeMachine.java
+++ Java/com/teamtreehouse/KaraokeMachine.java
@@ -1,5 +1,6 @@
 package com.teamtreehouse;
 
+import com.teamtreehouse.model.Song;
 import com.teamtreehouse.model.SongBook;
 import java.io.*;
 import java.util.HashMap;
@@ -26,6 +27,44 @@ public class KaraokeMachine {
       System.out.printf("%s - %s %n", option.getKey(), option.getValue());
     }
     System.out.print("What do you want to do: ");
+    String choice = mReader.readLine();
+    return choice.trim().toLowerCase();
+  }
+
+  public void run() {
+    String choice = "";
+    do {
+      try {
+        choice = promptAction();
+        switch(choice) {
+          case "add":
+            Song song = promptNewSong();
+            mSongBook.addSong(song);
+            System.out.printf("%s added.  %n%n%n, song");
+            break;
+          case "quit":
+            System.out.println("Thanks for playing!");
+            break;
+          default:
+            System.out.printf("Unknown choice: %s. Try Again. %n%n%n", choice);
+
+        }
+      }catch (IOException ioe) {
+        System.out.println("Problem with input.");
+        ioe.printStackTrace();
+      }
+
+    }while (!choice.equals("quit"));
+  }
+
+  private Song promptNewSong() throws IOException {
+    System.out.print("Enter the artist's name:  ");
+    String artist = mReader.readLine();
+    System.out.print("Enter the title:  ");
+    String title = mReader.readLine();
+    System.out.print("Enter the video URL:  ");
+    String videoUrl = mReader.readLine();
+    return new Song(artist, title, videoUrl);
 
   }
 }
