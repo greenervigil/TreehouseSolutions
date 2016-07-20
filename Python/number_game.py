ec9f8aa703e5f3173202a5f90f991395f14d400b
Daniel Vigil <daniel.julian.vigil@gmail.com>
2016-07-20 16:42:28 -0400

    modified number game

--- Python/number_game.py
+++ Python/number_game.py
@@ -1,15 +1,34 @@
 import random
 
-# generate random number between 1 and 10
-secret_num = random.randint(1, 10)
 
-while True:
-    # get a number from player
-    guess = int(input("Guess a number between 1 and 10  "))
-    # compare guess
-    #print hit/miss
-    if guess == secret_num:
-        print("You got it!!!  My number was {}".format(secret_num))
-        break
+
+def game():
+    # generate random number between 1 and 10
+    secret_num = random.randint(1, 10)
+    guesses = []
+
+    while len(guesses) < 5:
+        try:
+            # get a number from player
+            guess = int(input("Guess a number between 1 and 10  "))
+        except ValueError:
+            print("{} is not a number.".format(guess))
+        else:
+
+            # compare guess
+            #print hit/miss
+            if guess == secret_num:
+                print("You got it!!!  My number was {}".format(secret_num))
+                break
+            elif guess < secret_num :
+                print("My number is higher than {}".format(guess))
+            else:
+                print("My number is lower than {}".format(guess))
+            guesses.append(guess)
     else:
-        print("That is not it!")
+        print("You lose.  My number was {}".format(secret_num))
+    play_again = input("Do you want to play again? Y/n ")
+    if play_again.lower() != 'n':
+        game()
+    else:
+game()
